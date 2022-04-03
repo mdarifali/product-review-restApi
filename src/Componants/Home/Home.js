58c1d793391117149bce0cc7feab32a31e748e9c
mdarifali <arif.raj2050@gmail.com.com>
2022-04-03 23:35:02 +0600

    code setup

---
+++ src/Componants/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6'>
+
+                </div>
+                <div className='col-md-6'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
