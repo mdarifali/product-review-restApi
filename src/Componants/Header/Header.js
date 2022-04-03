8a238e66c45fcdc1cd396753c48fa3838eefe323
mdarifali <arif.raj2050@gmail.com.com>
2022-04-04 00:36:22 +0600

    home page done

--- src/Componants/Header/Header.js
+++ src/Componants/Header/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+            <div className="container">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/home">Home</a>
                         </li>
--- src/Componants/Home/Home.js
+++ src/Componants/Home/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Img from '../img/pizza-2.jpg'
 
 const Home = () => {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-md-6'>
-
-                </div>
-                <div className='col-md-6'>
-                    <img src="" alt="" />
+        <div className='container my-5'>
+            <div className="card mb-3 border-0">
+                <div className="row g-4">
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h1 className="card-title my-3">New Cheese <span classNameName='text-danger fw-bold'>BURST</span></h1>
+                            <h2 className="card-title">Super Cheesy <br />Super Tasty</h2>
+                            <p className="card-text my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Soluta optio nulla fugiat, placeat laudantium molestias voluptas veritatis eius tenetur quidem eos ratione laboriosam ut libero.</p>
+                            <a href="#" className="btn btn-success my-3">ORDER NOW</a>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <img src={Img} className="img-fluid rounded-start" alt="..." />
+                    </div>
                 </div>
             </div>
         </div>
